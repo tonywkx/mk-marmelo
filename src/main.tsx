74870fc27dd29ff833a40c814839d417f81d024e
Tony Kolesnikov <tonyk@Mac.Dlink>
2025-05-27 22:56:45 +0400

    feat: gh pages preparation

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/mk-marmelo">
     <App />
   </BrowserRouter>
 );
