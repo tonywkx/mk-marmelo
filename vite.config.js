1e8a1a3c5257e3e9a49c31e92839f793585f1006
Anna Chernysheva <149769181+Ann4j@users.noreply.github.com>
2025-06-02 18:44:09 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api/hubspot': {
-        target: 'https://api.hubapi.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/hubspot/, ''),
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        }
-      }
-    }
-  }
+  plugins: [react(), tailwindcss()]
 })
